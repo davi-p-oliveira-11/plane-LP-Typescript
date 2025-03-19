536f26ef93183233060c7aa9756f9dcb552c2402
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-19 15:28:53 -0300

    installed shadcn ui and adjusted some variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Fieldguide",
   description: "Effortless, secure, and intelligent audit management.",
 };
+
+const inter = Inter({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -13,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-gray-900 text-white">{children}</body>
+    <html lang="en" className={`${inter.variable}`}>
+      <body className="bg-blue-background text-white">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Button from "./components/Button";
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <div className="text-white">Hello world!</div>
+    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+      <Navbar />
+      <Hero />
 
       <Button text="Hallo" />
     </main>
