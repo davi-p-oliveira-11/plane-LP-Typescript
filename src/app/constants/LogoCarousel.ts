acbdbdd67d3927470108c857fe70181a3a49ee71
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-27 17:35:25 -0300

    updated  logo carousel data

---
+++ src/app/constants/LogoCarousel.ts
@@ -0,0 +1,14 @@
+const logos = [
+  "/images/assets/aprio.svg",
+  "/images/assets/berrydunn.svg",
+  "/images/assets/cbiz.svg",
+  "/images/assets/cri.svg",
+  "/images/assets/hoolbrook.svg",
+  "/images/assets/lbmc.svg",
+  "/images/assets/aprio.svg",
+  "/images/assets/mazars.svg",
+  "/images/assets/mlr.svg",
+  "/images/assets/warren-averett.svg",
+  "/images/assets/weaver.svg",
+  "/images/assets/wipfli.svg",
+];
