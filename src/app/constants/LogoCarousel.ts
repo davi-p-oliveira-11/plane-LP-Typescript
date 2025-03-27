6193ca80704ba8db4b698bad22bb5dc7b3b9f24e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-27 19:49:23 -0300

    updated companies section

--- src/app/constants/LogoCarousel.ts
+++ src/app/constants/LogoCarousel.ts
@@ -1,15 +1,11 @@
 import { Logo } from "../types/LogoCaroulsel";
 
 export const logos: Logo[] = [
-  "/images/assets/aprio.svg",
   "/images/assets/berrydunn.svg",
   "/images/assets/cbiz.svg",
   "/images/assets/cri.svg",
   "/images/assets/hoolbrook.svg",
   "/images/assets/lbmc.svg",
-  "/images/assets/aprio.svg",
-  "/images/assets/mazars.svg",
-  "/images/assets/mlr.svg",
   "/images/assets/warren-averett.svg",
   "/images/assets/weaver.svg",
   "/images/assets/wipfli.svg",
--- src/app/sections/Companies.tsx
+++ src/app/sections/Companies.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoCarousel from "../components/LogoCarousel";
+import { logos } from "../constants/LogoCarousel";
 
 export default function Companies() {
   return (
@@ -8,7 +8,17 @@ export default function Companies() {
         Top 500 firms trust Fieldguide to improve efficiency, revenue, and
         client experiences
       </p>
-      <LogoCarousel />
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-8 gap-8 justify-items-center">
+        {logos.map((logo, index) => (
+          <div key={index} className="flex justify-center items-center">
+            <img
+              src={logo}
+              alt={`Logo ${index}`}
+              className="w-24 h-24 object-contain"
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
