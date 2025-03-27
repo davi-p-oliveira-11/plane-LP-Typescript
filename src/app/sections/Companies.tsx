b7736029d27c3b21a254017af87545932cf9ead2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-27 17:13:21 -0300

    updated companies section text

--- src/app/sections/Companies.tsx
+++ src/app/sections/Companies.tsx
@@ -3,9 +3,12 @@ import LogoCarousel from "../components/LogoCarousel";
 
 export default function Companies() {
   return (
-    <>
-      <p>text</p>
+    <section className="w-full px-4 py-16 md:py-24 flex flex-col items-center text-center">
+      <p>
+        Top 500 firms trust Fieldguide to improve efficiency, revenue, and
+        client experiences
+      </p>
       <LogoCarousel />
-    </>
+    </section>
   );
 }
