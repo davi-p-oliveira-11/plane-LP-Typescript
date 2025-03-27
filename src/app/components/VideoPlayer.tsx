277a0ca1dcb0dff16aa6e64520a9c4ef0b1f7714
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-27 16:41:06 -0300

    updated mock video

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -1,5 +1,11 @@
+"use client";
 import React from "react";
 
+import { useRef, useState } from "react";
+
 export default function VideoPlayer() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPLaying] = useState(false);
+
   return <div>VideoPlayer</div>;
 }
